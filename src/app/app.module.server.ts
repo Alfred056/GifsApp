339f786fd375db196433c15d2aa909e3f7e01263
Alfred056 <alfredovilca374@gmail.com>
2024-06-15 03:06:28 -0500

    add main and sidebar component

--- src/app/app.module.server.ts
+++ src/app/app.module.server.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
-
+import { FormsModule } from '@angular/forms';
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
     AppModule,
+    FormsModule,
     ServerModule,
   ],
   bootstrap: [AppComponent],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainComponent } from './main/main.component';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ButtonModule
   ],
   providers: [
     provideClientHydration()
